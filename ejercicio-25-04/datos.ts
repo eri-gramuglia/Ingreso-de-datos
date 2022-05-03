f00df5da658c20ed4b7b0bee3b61f9bb21d0d089
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-03 18:53:50 -0300

    codigo-definitivo ordenamiento de repositores

--- ejercicio-25-04/datos.ts
+++ ejercicio-25-04/datos.ts
@@ -3,8 +3,7 @@ let dato = document.getElementById("dato");
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click", () => {
-  // TU CÓDIGO VA AQUÍ, POR EJEMPLO:
+  
   console.log("el dato ingresado es:", dato.value);
 
-  //
-});
+  });
