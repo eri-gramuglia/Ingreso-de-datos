d946902cb83991cef32de51154c869a9d3e11803
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-03 14:52:16 -0300

    codigo a definir organizacion de repositorio

---
+++ ejercicio-25-04/datos.ts
@@ -0,0 +1,10 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  // TU CÓDIGO VA AQUÍ, POR EJEMPLO:
+  console.log("el dato ingresado es:", dato.value);
+
+  //
+});
